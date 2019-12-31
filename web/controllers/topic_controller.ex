23924c040ef4ca24058f7b0746f721ff9b2ffc1b
John Joseph <atiquajohn@gmail.com>
2019-12-31 14:07:44 +0700

    completed authentication

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,6 +4,7 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug(Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :delete, :update])
+  plug(:check_topic_owner when action in [:update, :edit, :delete])
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
@@ -60,5 +61,18 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot perform this action")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
